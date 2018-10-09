8460ec0875edb8917b6e1eef51e6a613e85d8289
Serene Matheson <joh13085@byui.edu>
2018-10-09 09:20:59 -0700

    update css

--- web/shopping-cart/confirmation.php
+++ web/shopping-cart/confirmation.php
@@ -4,14 +4,14 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
 
 <html>
 <body>
-
+<div class="center">
 Thank you for your purchase <?php echo $_GET["name"]; ?>!<br>
 You have purchased: <br>
-Your items will be sent to this address: 
-<?php echo $_GET["street"]; ?> 
+Your items will be sent to the following address:<br>
+<?php echo $_GET["street"]; ?><br>
 <?php echo $_GET["city"]; ?>, 
-<?php echo $_GET["state"]; ?>
+<?php echo $_GET["state"]; ?> 
 <?php echo $_GET["zip"]; ?>
-
+</div>
 </body>
 </html>
