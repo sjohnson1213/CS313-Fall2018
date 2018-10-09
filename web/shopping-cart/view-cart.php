640bdaea851f023298f92f94003cda28a1fba921
Serene Matheson <joh13085@byui.edu>
2018-10-09 16:52:02 -0700

    adding SESSION

--- web/shopping-cart/view-cart.php
+++ web/shopping-cart/view-cart.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
+session_start(); 
 echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
+
+if (!isset($_SESSION["cart"])) {
+    $_SESSION["cart"] = 0;
+}
+
+foreach ($sets as $set) {
+    $_SESSION["cart"]
+}
 ?>
 
 <html>
@@ -8,16 +17,15 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
     </head>
     <body>
         <div class="center">
-            <h1 class="heading">Cart</h1>
+            <h1 class="heading">Your Cart</h1>
             <?php
-                $sets= $_POST["sets"];
-                foreach($sets as $set) {
-                     echo "$set<br>";
-                }
+                $items = $_SESSION["cart"];
+                foreach($items as $item)
+                echo "$item<br>";
             ?>
             <br>
             <input type="button" value="Back to Browse" onclick="window.location.href='browse.php'" class="button"/>
-            <input type="button" value="Continue to Checkout" onclick="window.location.href='checkout.php'" class="button"/>   
+            <input type="button" value="Continue to Checkout" onclick="window.location.href='checkout.php'" class="button" />   
         </div>
     </body>
 </html>
