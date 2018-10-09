ba2c059d7fb5f974f7b7a3ee10a7bb457163f7f3
Serene Matheson <joh13085@byui.edu>
2018-10-09 09:24:18 -0700

    fixing confirmation style

--- web/shopping-cart/confirmation.php
+++ web/shopping-cart/confirmation.php
@@ -5,13 +5,14 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
 <html>
 <body>
 <div class="center">
-Thank you for your purchase <?php echo $_GET["name"]; ?>!<br>
-You have purchased: <br>
-Your items will be sent to the following address:<br>
+<h1>Thank you for your purchase <?php echo $_GET["name"]; ?>!</h1><br><br>
+<h2>You have purchased: </h2><br><br>
+<h3>Your items will be sent to the following address:<br>
 <?php echo $_GET["street"]; ?><br>
 <?php echo $_GET["city"]; ?>, 
 <?php echo $_GET["state"]; ?> 
 <?php echo $_GET["zip"]; ?>
+</h3>   
 </div>
 </body>
 </html>
