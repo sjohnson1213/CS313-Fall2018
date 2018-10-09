9f534190fe6a843df3f13942ff92c566afccd67d
Serene Matheson <joh13085@byui.edu>
2018-10-09 11:19:02 -0700

    troubleshoot

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -1,28 +1,8 @@
-<?php session_start();
-
-//Set up products, images, and cost
-$images = array("images/hulkbuster.jpeg", "images/quest.jpeg", "images/ultimate-battle.jpeg", "images/antman-wasp.jpeg", "images/black-panther.jpeg", "images/beware.jpeg");
-$products = array("Hulkbuster", "Thor's Weapon Quest", "Ultimate Battle", "Ant-Man and the Wasp", "Black Panther/Rhino", "Beware the Vulture");
-$amounts = array("23.99", "19.99", "69.99", "16.99", "15.99", "25.99");
+<?php 
+session_start();
 echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
- 
-if ( !isset($_SESSION["total"]) ) {
-    $_SESSION["total"] = 0;
-    for ($i=0; $i< count($products); $i++) {
-     $_SESSION["quantity"][$i] = 0;
-    $_SESSION["amounts"][$i] = 0;
-   }
-  }
-
- if ( isset($_GET["add"]) )
-   {
-   $i = $_GET["add"];
-   $quantity = $_SESSION["quantity"][$i] + 1;
-   $_SESSION["amounts"][$i] = $amounts[$i] * $quantity;
-   $_SESSION["cart"][$i] = $i;
-   $_SESSION["quantity"][$i] = $quantity;
- }
 ?>
+
 <html>
     <head>
         <title>Product Page</title>
--- web/shopping-cart/view-cart.php
+++ web/shopping-cart/view-cart.php
@@ -10,3 +10,4 @@ foreach($sets as $set) {
     echo $set;
     <br>
 }
+?>
