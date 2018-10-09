a0288efd106e149ecf1ea6b2a9770263e9d6f0c6
Serene Matheson <joh13085@byui.edu>
2018-10-09 11:26:16 -0700

    troubleshoot add to cart

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -24,32 +24,32 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
     <tr>
         <td>Hulkbuster<br><img src="images/hulkbuster.jpeg"></td>
         <td>23.99</td>
-        <td><input type="checkbox" name="sets[]" value="hulk">
+        <td><input type="checkbox" name="sets[]" value="Hulkbuster Lego Set"></td>
     </tr>
     <tr>
         <td>Thor's Weapon Quest<br><img src="images/quest.jpeg"></td>
         <td>19.99</td>
-        <td><input type="checkbox" name="sets[]" value="thor">
+        <td><input type="checkbox" name="sets[]" value="Thor's Weapon Quest Lego Set"></td>
     </tr>
     <tr>
         <td>Ultimate Battle<br><img src="images/ultimate-battle.jpeg"></td>
         <td>69.99</td>
-        <td><input type="checkbox" name="sets[]" value="battle">
+        <td><input type="checkbox" name="sets[]" value="Ultimate Battle Lego Set"></td>
     </tr>
     <tr>
         <td>Ant-Man and the Wasp<br><img src="images/antman-wasp.jpeg"></td>
         <td>16.99</td>
-        <td><input type="checkbox" name="sets[]" value="antman">
+        <td><input type="checkbox" name="sets[]" value="AntMan and the Wasp Lego Set"></td>
     </tr>
     <tr>
         <td>Black Panther VS Rhino<br><img src="images/black-panther.jpeg"></td>
         <td>15.99</td>
-        <td><input type="checkbox" name="sets[]" value="panther">
+        <td><input type="checkbox" name="sets[]" value="Black Panther VS Rhino Lego Set"></td>
     </tr>
     <tr>
         <td>Beware the Vulture<br><img src="images/beware.jpeg"></td>
         <td>25.99</td>
-        <td><input type="checkbox" name="sets[]" value="vulture">
+        <td><input type="checkbox" name="sets[]" value="Beware the Vulture Lego Set"></td>
     </tr>
 </table>
 <input type="submit" value="Add To Cart">
