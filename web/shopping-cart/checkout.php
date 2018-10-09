55831112d7c4cbfc2667f91eacf019f0f68de546
Serene Matheson <joh13085@byui.edu>
2018-10-09 09:13:47 -0700

    add confirmation page template

--- web/shopping-cart/checkout.php
+++ web/shopping-cart/checkout.php
@@ -4,17 +4,18 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
 <html> 
 <head>
 <link href="https://fonts.googleapis.com/css?family=Fahkwang" rel="stylesheet">
+<style> 
+</style>
 </head>
-
 <body>
     <div class="center">
     <h1 id="checkout">Checkout</h1>
     <form action="confirmation.php" method="post">
-        Name: <input type="text" name="name" float="right"><br><br>
-        Street Address: <input type="text" name="street" style="direction: rtl;"><br><br>
-        City: <input type="text" name="city" style="direction: rtl;"><br><br>
-        State: <input type="text" name="state" style="direction: rtl;"><br><br>
-        Zip Code: <input type="text" name="zip" style="direction: rtl;"><br><br>
+        Name: <input type="text" name="name"><br><br>
+        Street Address: <input type="text" name="street"><br><br>
+        City: <input type="text" name="city"><br><br>
+        State: <input type="text" name="state"><br><br>
+        Zip Code: <input type="text" name="zip"><br><br>
         <input type="submit" style="float: left">
     </form>
     </div>
--- web/shopping-cart/confirmation.php
+++ web/shopping-cart/confirmation.php
@@ -1,3 +1,17 @@
 <?php 
 echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
 ?>
+
+<html>
+<body>
+
+Thank you for your purchase <?php echo $_GET["name"]; ?>!<br>
+You have purchased: <br>
+Your items will be sent to this address: 
+<?php echo $_GET["street"]; ?> 
+<?php echo $_GET["city"]; ?>, 
+<?php echo $_GET["state"]; ?>
+<?php echo $_GET["zip"]; ?>
+
+</body>
+</html>
