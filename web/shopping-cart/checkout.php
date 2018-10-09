0f6d472b08e1386f53526b54517cb23e8627236e
Serene Matheson <joh13085@byui.edu>
2018-10-09 07:25:45 -0700

    update css

--- web/shopping-cart/checkout.php
+++ web/shopping-cart/checkout.php
@@ -3,15 +3,16 @@ echo "<link rel='stylesheet' type='text/css' href='mainstyle.css' />";
 ?>
 <html> 
 <body>
-
-<form action="confirmation.php" method="post">
-Name: <input type="text" name="name" float="right"><br><br>
-Street Address: <input type="text" name="street" style="direction: rtl;"><br><br>
-City: <input type="text" name="city" style="direction: rtl;"><br><br>
-State: <input type="text" name="state" style="direction: rtl;"><br><br>
-Zip Code: <input type="text" name="zip" style="direction: rtl;"><br><br>
-<input type="submit" style="float: left">
-</form>
-
+    <div class="center">
+    <h1>Checkout</h1>
+    <form action="confirmation.php" method="post">
+        Name: <input type="text" name="name" float="right"><br><br>
+        Street Address: <input type="text" name="street" style="direction: rtl;"><br><br>
+        City: <input type="text" name="city" style="direction: rtl;"><br><br>
+        State: <input type="text" name="state" style="direction: rtl;"><br><br>
+        Zip Code: <input type="text" name="zip" style="direction: rtl;"><br><br>
+        <input type="submit" style="float: left">
+    </form>
+    </div>
 </body>
 </html>
