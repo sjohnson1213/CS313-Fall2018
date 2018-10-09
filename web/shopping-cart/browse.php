de6d826a63c9c64947d014f2e30d9cb9f7153500
Serene Matheson <joh13085@byui.edu>
2018-10-08 21:55:26 -0700

    Beginning of Shopping Cart

---
+++ web/shopping-cart/browse.php
@@ -0,0 +1,25 @@
+<?php session_start();
+
+$images = array("images/hulkbuster.jpeg", "images/quest.jpeg", "images/ultimate-battle.jpeg", "images/antman-wasp.jpeg", "images/black-panther.jpeg", "images/beware.jpeg");
+$products = array("Hulkbuster", "Thor's Weapon Quest", "Ultimate Battle", "Ant-Man and the Wasp", "Black Panther/Rhino", "Beware the Vulture");
+$amounts = array("23.99", "19.99", "69.99", "16.99", "15.99", "25.99");
+?>
+
+<h1>Lego Sets</h1>
+<table>
+    <tr>
+    <th>Product</th>
+    <th>Amount</th>
+    <th>Add to Cart?</th>
+    </tr>
+<?php
+for ($i=0; $i<$count(products); $i++){
+    ?>
+    <tr>
+    <td><?php echo($products[$i]);?><br><?php echo($images[$i]); ?></td>
+    <td><?php echo($amounts[$i]);?></td>
+    <td>Add</td>
+    </tr>
+    <?php
+}
+?>
