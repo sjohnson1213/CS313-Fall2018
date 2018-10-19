97585e65c66d0d05ef5c913dbbe1f72ac1a211fb
Serene Matheson <joh13085@byui.edu>
2018-10-19 16:27:39 -0700

    app homepage template

---
+++ web/app_homepage.php
@@ -0,0 +1,33 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+
+<html>
+    <head>
+        <title>Project 1: Budget Application</title>
+        <link rel="stylesheet" type="text/css" href="mainstyle.css">
+    </head>
+    <body>
+        <h1>Your Budget</h1>
+    </body>
+</html>
