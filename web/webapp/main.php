6de3f00739cb5b845e0af4fdb7ade1be42ed201e
Serene Matheson <joh13085@byui.edu>
2018-10-29 22:32:14 -0700

    troubleshoot

--- web/webapp/main.php
+++ web/webapp/main.php
@@ -40,7 +40,7 @@ catch (PDOException $ex)
     <body>
         <button type="button" onclick="window.location.href='transactions.php'" id="tran_button">Go to Transactions</button>
         <div class="center">
-            <div>Hello,
+            <div id="welcome">Hello,
             <?php
             foreach ($db->query("SELECT name FROM budget_user WHERE username='$username' AND password='$password'") as $row)
             {
@@ -48,7 +48,39 @@ catch (PDOException $ex)
             }
             ?>
             </div>
-            <h1 class="page_header">Your Budget</h1>
+            <h1 class="page_header">Your Budget</h1><br>
+            <table style="width:100%">
+            <tr>
+                <th>Income</th>
+                <th>Category</th> 
+                <th>Expected Expense</th>
+            </tr>
+            <tr>
+                <td>
+                <?php
+                foreach ($db->query("SELECT income FROM budget WHERE username='$username' AND password='$password'") as $row)
+                {
+                    echo $row['income'];
+                }
+                ?>
+                </td>
+                <td>
+                <?php
+                foreach ($db->query("SELECT category FROM budget WHERE username='$username' AND password='$password'") as $row)
+                {
+                    echo $row['category'];
+                }
+                ?>
+                </td> 
+                <td>
+                <?php
+                foreach ($db->query("SELECT expected_expenses FROM budget WHERE username='$username' AND password='$password'") as $row)
+                {
+                    echo $row['expected_expenses'];
+                }
+                ?>
+                </td>
+            </table>
 
         </div>
     </body>
